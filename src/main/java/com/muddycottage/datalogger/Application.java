1a077f1f4e81aa4c9bfcba86cffc40c8e06481b1
muddycottage <chris@muddycottage.com>
2021-03-11 16:56:27 +0000

    2

--- src/main/java/com/muddycottage/datalogger/Application.java
+++ src/main/java/com/muddycottage/datalogger/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
+@SpringBootApplication // = @Configuration + @ComponentScan + @EnableAutoConfiguration
 @EntityScan
 @EnableJpaRepositories
 public class Application {
---
+++ src/main/java/com/muddycottage/datalogger/web/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.muddycottage.datalogger.web;
+
+/*
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+*/
+
+//@EnableWebSecurity
+public class SecurityConfig {}
+        /*extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure();
+    }
+}
+*/
