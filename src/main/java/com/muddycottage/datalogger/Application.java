7e354f94e99e8765343debb8cf0ead72de3a9de4
muddycottage <chris@muddycottage.com>
2021-03-11 16:29:53 +0000

    debugging componentscan issue

--- src/main/java/com/muddycottage/datalogger/Application.java
+++ src/main/java/com/muddycottage/datalogger/Application.java
@@ -1,18 +1,12 @@
 package com.muddycottage.datalogger;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 @EntityScan
 @EnableJpaRepositories
 public class Application {
