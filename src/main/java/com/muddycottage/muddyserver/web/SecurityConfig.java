e9ed255bd8f9daac856bc4af69a59d81f6e88f37
muddycottage <chris@muddycottage.com>
2021-03-12 07:58:47 +0000

    minor tidy up

--- src/main/java/com/muddycottage/muddyserver/web/SecurityConfig.java
+++ src/main/java/com/muddycottage/muddyserver/web/SecurityConfig.java
@@ -1,13 +1,14 @@
 package com.muddycottage.muddyserver.web;
 
-/*
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-*/
 
-//@EnableWebSecurity
-public class SecurityConfig {}
-        /*extends WebSecurityConfigurerAdapter {
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,4 +18,5 @@ public class SecurityConfig {}
                 .requiresSecure();
     }
 }
-*/
+
+
